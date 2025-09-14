969759a6c6b6b0a0997ac29258b23f6c617f252b
Arigbo Jesse <105205668+Arigbo@users.noreply.github.com>
2025-09-14 23:55:04 +0100

    update

--- src/components/navCrombs.jsx
+++ src/components/navCrombs.jsx
@@ -1,13 +1,18 @@
-export  function NavCrumbs() {
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+export function NavCrumbs() {
+  const path = usePathname();
+
   return (
     <div className="breadcrumbs-container">
       <div className="breadcrumbs">
         <a href="/course">Courses</a>
         <span>&gt;</span>
-        <span>Frontend</span>
+        <span>{path === "/course/frontend" ? "Frontend" : "Backend"}</span>
       </div>
       <a className="next">
-        <a href="/course/backend">Backend Course</a>
+        <a href={path === "/course/frontend" ? "/course/backend" : "/course/frontend"}>{path === "/course/frontend" ? "Backend Course" : "Frontend Course"}</a>
         <i className="fas fa-chevron-right"></i>
       </a>
     </div>
