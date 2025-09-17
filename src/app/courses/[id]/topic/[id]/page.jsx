056ba10359b08df0fcbedf83d5f5c2486297a562
Arigbo Jesse <105205668+Arigbo@users.noreply.github.com>
2025-09-17 03:01:42 +0100

    fixed reason for fail deploment

--- src/app/courses/[id]/topic/[id]/page.jsx
+++ src/app/courses/[id]/topic/[id]/page.jsx
@@ -1,18 +1,18 @@
-"use client";
-import React from "react";
-import { useParams } from "next/navigation";
-const topics = {
-  1: {
-    id: 1,
-    topic: "How the Internet Works (Clients, Servers)",
-    intro: "",
-    title: "",
-    description: "",
-  },
-};
-const SingleTopic=()=>{
-  const { id } = useParams();
-  const topice = topics[id];
-  return <>{topice.topic}</>;
-}
-export default SingleTopic
+// "use client";
+// import React from "react";
+// import { useParams } from "next/navigation";
+// const topics = {
+//   1: {
+//     id: 1,
+//     topic: "How the Internet Works (Clients, Servers)",
+//     intro: "",
+//     title: "",
+//     description: "",
+//   },
+// };
+// const SingleTopic=()=>{
+//   const { id } = useParams();
+//   const topice = topics[id];
+//   return <>{topice.topic}</>;
+// }
+// export default SingleTopic
